afcef9682768dd9d106df14b08bfce41a3a2f2b6
Md. Abedur Rahman <40117792+Abed0711@users.noreply.github.com>
2019-12-08 23:47:17 +0600

    Database
    A little work

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from social.models import RecruterRegistration, CandidateRegistration, Category, CandidateProfile
 
-# Register your models here.
+admin.site.register(RecruterRegistration)
+admin.site.register(CandidateRegistration)
+admin.site.register(CandidateProfile)
+admin.site.register(Category)
--- social/models.py
+++ social/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class RecruterRegistration(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    user_name = models.CharField()
+    email = models.CharField()
+    password = models.CharField()
+    confirm_pass = models.CharField()
+    company_name = models.CharField()
+    company_url = models.URLField()
+    Profile_Pic = models.ImageField()
+
+    def __str__(self):
+        return self.user_name.title()
+
+
+class CandidateRegistration(models.Model):
+    candidate_id = models.IntegerField(primary_key=True)
+    first_name = models.CharField()
+    last_name = models.CharField()
+    candidate_user_name = models.CharField()
+    email = models.CharField()
+    password = models.CharField()
+    confirm_pass = models.CharField()
+    picture_user = models.ImageField()
+
+    def __str__(self):
+        return self.candidate_user_name.title()
+
+
+class Category(models.Model):
+    category_id = models.IntegerField(primary_key=True)
+    category_name = models.CharField()
+
+    def __str__(self):
+        return self.category_id
+
+
+class CandidateProfile(models.Model):
+    candidate_pro_id = models.ForeignKey(CandidateRegistration, on_delete=models.CASCADE)
+    candidate_category_id = models.ForeignKey(Candidate_registration, on_delete=models.CASCADE)
+    objective = models.TextField()
+    education = models.TextField()
+    personal_info = models.TextField()
+    projects = models.TextField()
+    technical_skill = models.TextField()
+    language = models.TextField()
+    awards = models.TextField()
+    membership = models.TextField()
+    interest = models.TextField()
+    hobbies = models.TextField()
+    personal_qualities = models.TextField()
+
+    def __str__(self):
+        return self.candidate_category_id
